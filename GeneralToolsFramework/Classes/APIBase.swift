14f983de30b236d96cc45573cdfe00e385358982
Zandor Smith <info@zsinfo.nl>
2018-12-10 18:54:47 +0100

    Added main utility classes.

---
+++ GeneralToolsFramework/Classes/APIBase.swift
@@ -0,0 +1,105 @@
+//
+//  APIBase.swift
+//  GeneralToolsFramework
+//
+//  Created by Zandor Smith on 10/12/2018.
+//
+
+import Foundation
+
+class APIBase {
+    
+    private let baseUrl: String
+    
+    init(baseUrl: String) {
+        self.baseUrl = baseUrl
+    }
+    
+    func doGetApiCall(url: String, onCompletion: @escaping (Data) -> (), onError: @escaping () -> ()) {
+        NetworkActivityHandler.pushNetworkActivity()
+        
+        URLSession.shared.dataTask(with: URL(string: baseUrl + url)!, completionHandler: {
+            (data, response, error) in
+            self.handleDataTask(data: data, response: response, error: error, onCompletion: { (data) in
+                onCompletion(data)
+            }, onError: {
+                onError()
+            })
+        }).resume()
+    }
+    
+    func doPostApiCall(url: String, postContent: [String: String], onCompletion: @escaping (Data) -> (), onError: @escaping () -> ()) {
+        NetworkActivityHandler.pushNetworkActivity()
+        
+        let postContentString = generatePostContentString(postContent: postContent)
+        if postContentString == nil {
+            onError()
+            return
+        }
+        
+        var request = URLRequest(url: URL(string: baseUrl + url)!)
+        request.httpMethod = "POST"
+        request.httpBody = postContentString!.data(using: .utf8)
+        
+        URLSession.shared.dataTask(with: request, completionHandler: {
+            (data, response, error) in
+            self.handleDataTask(data: data, response: response, error: error, onCompletion: { (data) in
+                onCompletion(data)
+            }, onError: {
+                onError()
+            })
+        }).resume()
+    }
+    
+    private func handleDataTask(data: Data?, response: URLResponse?, error: Error?, onCompletion: @escaping (Data) -> (), onError: @escaping () -> ()) {
+        if let _ = data, error != nil {
+            print("Network error.")
+            print("error=\(String(describing: error))")
+            NetworkActivityHandler.popNetworkActivity()
+            onError()
+            return
+        }
+        
+        if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+            print("Invalid http status code.")
+            print("statusCode should be 200, but is \(httpStatus.statusCode)")
+            print("response = \(String(describing: response))")
+            NetworkActivityHandler.popNetworkActivity()
+            onError()
+            return
+        }
+        
+        if data == nil {
+            NetworkActivityHandler.popNetworkActivity()
+            onError()
+            return
+        }
+        
+        let contents = String(data: data!, encoding: .utf8)
+        
+        if contents == nil {
+            NetworkActivityHandler.popNetworkActivity()
+            onError()
+            return
+        }
+        
+        print(contents!)
+        NetworkActivityHandler.popNetworkActivity()
+        onCompletion(data!)
+    }
+    
+    private func generatePostContentString(postContent: [String: String]) -> String? {
+        var string: String? = nil
+        for key in postContent.keys {
+            let value = postContent[key]!
+            let keyValuePair = key + "=" + value
+            if string == nil {
+                string = keyValuePair
+            } else {
+                string = string! + "&" + keyValuePair
+            }
+        }
+        return string
+    }
+    
+}
---
+++ GeneralToolsFramework/Classes/String+Hashing.swift
@@ -0,0 +1,65 @@
+//
+//  String+Hashing.swift
+//  GeneralToolsFramework
+//
+//  Created by Zandor Smith on 10/12/2018.
+//
+
+import Foundation
+
+// Credits:
+// https://stackoverflow.com/a/32166735/5544222
+// https://stackoverflow.com/a/25762128/5544222
+// https://stackoverflow.com/a/38788437/5544222
+extension String {
+    
+    func md5(string: String) -> Data {
+        let messageData = string.data(using:.utf8)!
+        var digestData = Data(count: Int(CC_MD5_DIGEST_LENGTH))
+        
+        _ = digestData.withUnsafeMutableBytes {digestBytes in
+            messageData.withUnsafeBytes {messageBytes in
+                CC_MD5(messageBytes, CC_LONG(messageData.count), digestBytes)
+            }
+        }
+        
+        return digestData
+    }
+    
+    func sha1() -> String {
+        let data = self.data(using: String.Encoding.utf8)!
+        var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA1($0, CC_LONG(data.count), &digest)
+        }
+        let hexBytes = digest.map { String(format: "%02hhx", $0) }
+        return hexBytes.joined()
+    }
+    
+    func sha256() -> String {
+        if let stringData = self.data(using: String.Encoding.utf8) {
+            return hexStringFromData(input: digest(input: stringData as NSData))
+        }
+        return ""
+    }
+    
+    private func digest(input : NSData) -> NSData {
+        let digestLength = Int(CC_SHA256_DIGEST_LENGTH)
+        var hash = [UInt8](repeating: 0, count: digestLength)
+        CC_SHA256(input.bytes, UInt32(input.length), &hash)
+        return NSData(bytes: hash, length: digestLength)
+    }
+    
+    private  func hexStringFromData(input: NSData) -> String {
+        var bytes = [UInt8](repeating: 0, count: input.length)
+        input.getBytes(&bytes, length: input.length)
+        
+        var hexString = ""
+        for byte in bytes {
+            hexString += String(format:"%02x", UInt8(byte))
+        }
+        
+        return hexString
+    }
+    
+}

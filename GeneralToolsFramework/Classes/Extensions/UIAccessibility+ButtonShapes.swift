45bdec9ca985c0a8fea3cf6cb43699eb42107ba6
Zandor Smith <info@zsinfo.nl>
2019-02-01 15:59:48 +0100

    Added check if button shapes is enabled.

---
+++ GeneralToolsFramework/Classes/Extensions/UIAccessibility+ButtonShapes.swift
@@ -0,0 +1,18 @@
+//
+//  UIAccessibility+ButtonShapes.swift
+//  GeneralToolsFramework
+//
+//  Created by Zandor Smith on 01/02/2019.
+//
+
+import UIKit
+
+extension UIAccessibility {
+    
+    public var isButtonShapesEnabled: Bool {
+        let button = UIButton()
+        button.setTitle("Button Shapes", for: .normal)
+        return button.titleLabel?.attributedText?.attribute(NSAttributedString.Key.underlineStyle, at: 0, effectiveRange: nil) != nil
+    }
+    
+}

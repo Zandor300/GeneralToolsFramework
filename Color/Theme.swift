1b8d3a5d253eeefefecdeddff098e116211ba162
Zandor Smith <info@zsinfo.nl>
2019-02-09 15:13:18 +0100

    Added a place to store the colors.

---
+++ Color/Theme.swift
@@ -0,0 +1,17 @@
+//
+//  Theme.swift
+//  Connectivity
+//
+//  Created by Zandor Smith on 09/02/2019.
+//
+
+import Foundation
+
+class Theme {
+    
+    public static let shared = Theme()
+    
+    public var primaryColor: UIColor = UIColor(red:0.00, green:0.48, blue:1.00, alpha:1.00)
+    public var secondaryColor: UIColor = UIColor.darkGray
+    
+}

6013494a1ced799ed2d5d605155bdf7dc57da437
Zandor Smith <info@zsinfo.nl>
2019-02-13 16:56:56 +0100

    Add function for additional padding.

--- GeneralToolsFramework/Classes/Extensions/MKMapView+Focus.swift
+++ GeneralToolsFramework/Classes/Extensions/MKMapView+Focus.swift
@@ -11,6 +11,16 @@ import MapKit
 // See https://stackoverflow.com/a/53025716/5544222
 extension MKMapView {
     
+    public func fitAll(animated: Bool, padding: UIEdgeInsets) {
+        var zoomRect            = MKMapRect.null;
+        for annotation in annotations {
+            let annotationPoint = MKMapPoint(annotation.coordinate)
+            let pointRect       = MKMapRect(x: annotationPoint.x, y: annotationPoint.y, width: 0.01, height: 0.01);
+            zoomRect            = zoomRect.union(pointRect);
+        }
+        setVisibleMapRect(zoomRect, edgePadding: UIEdgeInsets(top: 100 + padding.top, left: 100 + padding.left, bottom: 100 + padding.bottom, right: 100 + padding.right), animated: animated)
+    }
+    
     /// When we call this function, we have already added the annotations to the map, and just want all of them to be displayed.
     public func fitAll(animated: Bool) {
         var zoomRect            = MKMapRect.null;

2c159533e341adb4c93e9e8a3e78211f25316ea8
Zandor Smith <info@zsinfo.nl>
2019-03-04 01:09:47 +0100

    Document the Version class. Also remove the need to specify the parameter.

--- GeneralToolsFramework/Classes/Utilities/Version.swift
+++ GeneralToolsFramework/Classes/Utilities/Version.swift
@@ -9,9 +9,13 @@ import Foundation
 
 public class Version {
     
+    /**
+     Will return a new `Version` instance of the current app version.
+     - returns: New `Version` instance of the current app version.
+     */
     public static func getCurrentVersion() -> Version {
         let version = (Bundle.main.infoDictionary!["CFBundleShortVersionString"] as? String)! + "-" + (Bundle.main.infoDictionary!["CFBundleVersion"] as? String)!
-        return Version(version: version)
+        return Version(version)
     }
     
     var major: Int = 0
@@ -19,7 +23,18 @@ public class Version {
     var patch: Int = 0
     var buildNumber: Int = 0
     
-    public init(version: String) {
+    /**
+     Initiate a new `Version` instance of the provided version.
+     
+     Example usage:
+     ```swift
+     let version = Version("1.2.5-7")
+     ```
+     
+     - parameters:
+        - version: The version number. (Ex. "1.2-4" or "1.2.5-7" where the number behind the '-' is the build number.)
+     */
+    public init(_ version: String) {
         let split1 = version.split(separator: "-")
         buildNumber = Int(split1[1])!
         
@@ -31,7 +46,14 @@ public class Version {
         }
     }
     
-    public func compare(otherVersion: Version) -> VersionComparison {
+    /**
+     Will compare the current version with the other version that is provided. If `otherVersion` is higher than the current version, `VersionComparison.HIGHER` is returned. If they are equal, `VersionComparison.EQUAL` is returned and if `otherVersion` is lower, `VersionComparison.LOWER` is returned.
+     
+     - parameters:
+        - otherVersion: The version to compare the current one with.
+     - returns: The comparison result.
+     */
+    public func compare(with otherVersion: Version) -> VersionComparison {
         print("Comparing " + toString() + " with " + otherVersion.toString() + "...")
         if otherVersion.major > major {
             return VersionComparison.HIGHER
@@ -53,6 +75,11 @@ public class Version {
         return VersionComparison.LOWER
     }
     
+    /**
+     Will return a `String` of the version number. (Ex. "1.2.5-7")
+     
+     - returns: Version number as a `String`.
+     */
     public func toString() -> String {
         if patch != 0 {
             return String(major) + "." + String(minor) + "." + String(patch) + "-" + String(buildNumber)
@@ -60,6 +87,11 @@ public class Version {
         return String(major) + "." + String(minor) + "-" + String(buildNumber)
     }
     
+    /**
+     Will return a 'nice' `String` of the version number. Could be used for TestFlight or Debug builds on the settings page. (Ex. "1.2.5 (7)")
+     
+     - returns: Version number as a `String`.
+     */
     public func toNiceStringWithBuild() -> String {
         if patch != 0 {
             return String(major) + "." + String(minor) + "." + String(patch) + " (" + String(buildNumber) + ")"
@@ -67,6 +99,12 @@ public class Version {
         return String(major) + "." + String(minor) + " (" + String(buildNumber) + ")"
     }
     
+    
+    /**
+     Will return a 'nice' `String` of the version number. Could be used for App Store release builds on the settings page. (Ex. "1.2.5")
+     
+     - returns: Version number as a `String`.
+     */
     public func toNiceString() -> String {
         if patch != 0 {
             return String(major) + "." + String(minor) + "." + String(patch)

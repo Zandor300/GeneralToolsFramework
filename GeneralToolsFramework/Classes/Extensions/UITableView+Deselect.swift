744d2f6cea46fa3d773228094b43ab3cc3abdf79
Zandor Smith <info@zsinfo.nl>
2019-07-31 23:50:54 +0200

    Add UITableView deselecting all function.

---
+++ GeneralToolsFramework/Classes/Extensions/UITableView+Deselect.swift
@@ -0,0 +1,20 @@
+//
+//  UITableView+Deselect.swift
+//  GeneralToolsFramework
+//
+//  Created by Zandor Smith on 31/07/2019.
+//
+
+import UIKit
+
+public extension UITableView {
+
+    func deselectAllRows(animated: Bool) {
+        if let indexPaths = self.indexPathsForSelectedRows {
+            for indexPath in indexPaths {
+                self.deselectRow(at: indexPath, animated: animated)
+            }
+        }
+    }
+
+}

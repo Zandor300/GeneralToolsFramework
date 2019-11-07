fe2134258828fe7a49d885920d834e80c3e842a5
Zandor Smith <info@zsinfo.nl>
2019-11-07 15:20:16 +0100

    Add UIImage fixOrientation function.

---
+++ GeneralToolsFramework/Classes/Extensions/UIImage+Orientation.swift
@@ -0,0 +1,65 @@
+//
+//  UIImage+Orientation.swift
+//  GeneralToolsFramework
+//
+//  Created by Zandor Smith on 07/11/2019.
+//
+
+import UIKit
+
+extension UIImage {
+
+    /// Automatically rotate the UIImage to the orientation in the orientation exif so that that won't have to be done in the backend or on the clients that don't natively support the orientation exif.
+    func fixOrientation() -> UIImage {
+        // No-op if the orientation is already correct
+        if (self.imageOrientation == .up) {
+            return self
+        }
+
+        // We need to calculate the proper transformation to make the image upright.
+        // We do it in 2 steps: Rotate if left/right/down, and then flip if mirrored.
+        var transform: CGAffineTransform = .identity
+
+        if ( self.imageOrientation == .down || self.imageOrientation == .downMirrored ) {
+            transform = transform.translatedBy(x: self.size.width, y: self.size.height)
+            transform = transform.rotated(by: CGFloat(Double.pi))
+        }
+
+        if ( self.imageOrientation == .left || self.imageOrientation == .leftMirrored ) {
+            transform = transform.translatedBy(x: self.size.width, y: 0)
+            transform = transform.rotated(by: CGFloat(Double.pi / 2))
+        }
+
+        if ( self.imageOrientation == .right || self.imageOrientation == .rightMirrored ) {
+            transform = transform.translatedBy(x: 0, y: self.size.height);
+            transform = transform.rotated(by: CGFloat(-Double.pi / 2));
+        }
+
+        if (self.imageOrientation == .upMirrored || self.imageOrientation == .downMirrored) {
+            transform = transform.translatedBy(x: self.size.width, y: 0)
+            transform = transform.scaledBy(x: -1, y: 1)
+        }
+
+        if ( self.imageOrientation == .leftMirrored || self.imageOrientation == .rightMirrored ) {
+            transform = transform.translatedBy(x: self.size.height, y: 0)
+            transform = transform.scaledBy(x: -1, y: 1)
+        }
+
+        // Now we draw the underlying CGImage into a new context, applying the transform
+        // calculated above.
+        let cgImage = self.cgImage!
+        let ctx: CGContext = CGContext(data: nil, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: cgImage.bitsPerComponent, bytesPerRow: 0, space: cgImage.colorSpace!, bitmapInfo: cgImage.bitmapInfo.rawValue)!
+
+        ctx.concatenate(transform)
+
+        if (self.imageOrientation == .left || self.imageOrientation == .leftMirrored
+            || self.imageOrientation == .right || self.imageOrientation == .rightMirrored) {
+            ctx.draw(cgImage, in: CGRect(x: 0, y: 0, width: self.size.height, height: self.size.width))
+        } else {
+            ctx.draw(cgImage, in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        }
+
+        // And now we just create a new UIImage from the drawing context and return it
+        return UIImage(cgImage: ctx.makeImage()!)
+    }
+}

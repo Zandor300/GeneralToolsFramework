bba2008815c1a81c06bc866d57b710f2facad31f
Zandor Smith <info@zsinfo.nl>
2022-04-19 20:19:08 +0200

    Fix crash on LinkTableViewCell if text value is nil on UILabel. Allow setting using new text getsetter which also triggers a layout.

--- GeneralToolsFramework/Classes/iOS/Views/Table/LinkTableViewCell.swift
+++ GeneralToolsFramework/Classes/iOS/Views/Table/LinkTableViewCell.swift
@@ -11,6 +11,15 @@ public class LinkTableViewCell: UITableViewCell {
 
     public static var defaultLinkColor: UIColor = .systemBlue
 
+    public var text: String? {
+        get {
+            return textLabel?.text
+        }
+        set {
+            textLabel?.text = newValue
+            self.layoutSubviews()
+        }
+    }
     public var linkColor: UIColor = LinkTableViewCell.defaultLinkColor {
         didSet {
             self.layoutSubviews()
@@ -22,11 +31,11 @@ public class LinkTableViewCell: UITableViewCell {
         self.textLabel?.tintColor = linkColor
         self.imageView?.tintColor = linkColor
 
-        if UIAccessibility.isButtonShapesEnabled {
-            let textRange = NSRange(location: 0, length: self.textLabel!.text!.count)
-            let attributedText = NSMutableAttributedString(string: self.textLabel!.text!)
+        if UIAccessibility.isButtonShapesEnabled, let text = self.textLabel?.text {
+            let textRange = NSRange(location: 0, length: text.count)
+            let attributedText = NSMutableAttributedString(string: text)
             attributedText.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: textRange)
-            self.textLabel!.attributedText = attributedText
+            self.textLabel?.attributedText = attributedText
         }
     }
 

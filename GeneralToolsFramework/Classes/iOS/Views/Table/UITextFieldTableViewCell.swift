f2f30896ec1432a06c977d733ce073ea42fe7fde
Zandor Smith <info@zsinfo.nl>
2023-11-29 15:27:14 +0100

    Add option to UITextFieldTableViewCell to select all UITextField content upon begin editing.

--- GeneralToolsFramework/Classes/iOS/Views/Table/UITextFieldTableViewCell.swift
+++ GeneralToolsFramework/Classes/iOS/Views/Table/UITextFieldTableViewCell.swift
@@ -11,6 +11,7 @@ import UIKit
 public class UITextFieldTableViewCell: UITableViewCell {
 
     public var textField = UITextField()
+    public var selectAllOnBeginEditing: Bool = false
 
     public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -34,11 +35,23 @@ public class UITextFieldTableViewCell: UITableViewCell {
         textField.textAlignment = .right
         textField.borderStyle = .none
         textField.frame = CGRect(x: 0, y: 0, width: 150, height: 20)
+        textField.delegate = self
 
         self.selectionStyle = .none
         self.accessoryView = textField
         self.setNeedsLayout()
         self.layoutIfNeeded()
+    }
+
+}
+
+extension UITextFieldTableViewCell: UITextFieldDelegate {
+
+    public func textFieldDidBeginEditing(_ textField: UITextField) {
+        guard selectAllOnBeginEditing else {
+            return
+        }
+        textField.perform(#selector(textField.selectAll(_:)), with: nil, afterDelay: 0.05)
     }
 
 }

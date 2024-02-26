34fa9822f9bd66c982c65f21c554758c3ac490e6
Zandor Smith <info@zsinfo.nl>
2024-02-26 08:37:09 -0500

    Add async method for dismissing presented view controllers.

--- GeneralToolsFramework/Classes/iOS/Extensions/UIViewController+Dismiss.swift
+++ GeneralToolsFramework/Classes/iOS/Extensions/UIViewController+Dismiss.swift
@@ -10,7 +10,7 @@ import UIKit
 
 extension UIViewController {
 
-    // Dismiss all presented view controllers that were presented from this one and childs from this until the current one is shown. onCompletion is called when done.
+    /// Dismiss all presented view controllers that were presented from this one and childs from this until the current one is shown. onCompletion is called when done.
     public func dismissViewControllers(animated: Bool, onCompletion: (() -> Void)?) {
         guard let viewController = self.presentingViewController else {
             if let handler = onCompletion {
@@ -26,6 +26,16 @@ extension UIViewController {
                         handler()
                     }
                 }
+            }
+        }
+    }
+
+    /// Dismiss all presented view controllers that were presented from this one and childs from this until the current one is shown. onCompletion is called when done.
+    @available(iOS 13.0, *)
+    public func dismissViewControllers(animated: Bool) async {
+        return await withCheckedContinuation { continuation in
+            self.dismissViewControllers(animated: animated) {
+                continuation.resume()
             }
         }
     }
